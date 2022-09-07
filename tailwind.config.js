d96ddb67a2c8cfff6a60d0d04165ca3d2fe0ad32
jibrilmohamod <jazimjibril@gmail.com>
2022-09-07 18:02:03 +0300

    project init

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+ content: ["./public/**/*.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
+ theme: {
+  extend: {
+   fontFamily: {
+    sans: ["Cartograph CF"],
+   },
+  },
+ },
+ plugins: [],
+}
