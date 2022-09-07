53810971952e623172c45542100885714fdde8e4
jibrilmohamod <jazimjibril@gmail.com>
2022-09-07 21:51:42 +0300

    Created Hero Section

---
+++ src/components/TheHero.vue
@@ -0,0 +1,9 @@
+<template>
+ <div
+  class="bg-[url('../assets/images/HeroImage.png')] h-[87vh] w-full border border-green-500 bg-center"
+ ></div>
+</template>
+
+<script setup></script>
+
+<style lang="scss" scoped></style>
