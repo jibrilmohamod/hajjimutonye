a100c51b3508731576c2078ced302a94aacc8002
jibrilmohamod <jazimjibril@gmail.com>
2022-09-07 18:05:18 +0300

    created some views

---
+++ src/components/TheName.vue
@@ -0,0 +1,13 @@
+<template>
+ <div class="border border-green-500">
+  <router-link to="/">
+   <img src="../assets/images/Name.svg" alt="" />
+  </router-link>
+ </div>
+</template>
+
+<script setup>
+ import { RouterLink } from "vue-router"
+</script>
+
+<style lang="scss" scoped></style>
---
+++ src/components/TheNav.vue
@@ -0,0 +1,15 @@
+<template>
+ <div>
+  <nav class="border flex justify-center">
+   <RouterLink to="/" class="px-7">WORK</RouterLink>
+   <RouterLink to="/about" class="px-7">DEEP DIVE</RouterLink>
+   <RouterLink to="/about" class="px-7">JOURNAL</RouterLink>
+  </nav>
+ </div>
+</template>
+
+<script setup>
+ import { RouterLink } from "vue-router"
+</script>
+
+<style lang="scss" scoped></style>
