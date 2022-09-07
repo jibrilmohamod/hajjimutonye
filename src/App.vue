6df20b986e129a25d53ecbfb2383c15dafd8ed04
jibrilmohamod <jazimjibril@gmail.com>
2022-09-07 18:04:14 +0300

    project init

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<script setup>
+ import { RouterView } from "vue-router"
+ import TheName from "./components/TheName.vue"
+ import TheNav from "./components/TheNav.vue"
+</script>
+
+<template>
+ <main class="">
+  <header>
+   <TheName></TheName>
+   <TheNav></TheNav>
+  </header>
+ </main>
+
+ <RouterView />
+</template>
