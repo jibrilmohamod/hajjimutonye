b1f057f21868a1a79bfe92838643372d289f9670
jibrilmohamod <jazimjibril@gmail.com>
2022-09-08 16:35:42 +0300

    added projects view

---
+++ src/components/TheWorks.vue
@@ -0,0 +1,12 @@
+<template>
+ <div class="h-screen w-full bg-zinc-900 px-32">
+  <!-- title -->
+  <div class="w-full flex justify-center">
+   <p class="text-5xl text-white">MY WORKS</p>
+  </div>
+ </div>
+</template>
+
+<script setup></script>
+
+<style lang="scss" scoped></style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,12 +2,14 @@
  <div>
   <TheHero></TheHero>
   <TheProfile></TheProfile>
+  <TheWorks></TheWorks>
  </div>
 </template>
 
 <script setup>
  import TheHero from "../components/TheHero.vue"
  import TheProfile from "../components/TheProfile.vue"
+ import TheWorks from "../components/TheWorks.vue"
 </script>
 
 <style lang="scss" scoped></style>
