84d282ef9cec5dcc505dc05e161dd7f8ffced956
jibrilmohamod <jazimjibril@gmail.com>
2022-09-08 12:54:26 +0300

    added an svg

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,14 @@
 <script setup>
  import { RouterView } from "vue-router"
  import TheName from "./components/TheName.vue"
- import TheNav from "./components/TheNav.vue"
+ //  import TheNav from "./components/TheNav.vue"
 </script>
 
 <template>
  <main class="">
   <header>
    <TheName></TheName>
-   <TheNav></TheNav>
+   <!-- <TheNav></TheNav> -->
   </header>
  </main>
 
