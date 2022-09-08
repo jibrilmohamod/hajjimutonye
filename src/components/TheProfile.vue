9c052e6a3c027692a82aa4dce89d7713ee7ba6c9
jibrilmohamod <jazimjibril@gmail.com>
2022-09-08 14:31:40 +0300

    added a profile component

---
+++ src/components/TheProfile.vue
@@ -0,0 +1,12 @@
+<template>
+ <div class="h-[100vh] bg-zinc-900 w-full px-32 pt-20">
+  <p class="text-5xl text-white">WHO IS HAJI MUTONYE?</p>
+  <div class="h-96 w-96 bg-slate-400 rounded-full ml-28">
+   <img src="" alt="" class="h-full w-full rounded-full" />
+  </div>
+ </div>
+</template>
+
+<script setup></script>
+
+<style lang="scss" scoped></style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,11 +1,13 @@
 <template>
  <div>
   <TheHero></TheHero>
+  <TheProfile></TheProfile>
  </div>
 </template>
 
 <script setup>
  import TheHero from "../components/TheHero.vue"
+ import TheProfile from "../components/TheProfile.vue"
 </script>
 
 <style lang="scss" scoped></style>
