5e2921a17e0e18b6f1d3fad0f2b08eb84a03504f
jibril <jazimjibril@gmail.com>
2022-09-14 22:49:42 +0300

    work in progress

--- src/components/TheWorks.vue
+++ src/components/TheWorks.vue
@@ -1,12 +1,38 @@
 <template>
- <div class="h-screen w-full bg-blue-900 px-32">
-  <!-- title -->
-  <div class="w-full flex justify-center">
-   <p class="text-5xl text-white">MY WORKS</p>
+  <div class="h-screen w-full bg-blue-900 px-32">
+    <!-- title -->
+    <div class="w-full flex justify-center">
+      <p class="text-5xl text-white">MY WORKS</p>
+    </div>
+    <!-- works -->
+    <div class="h-full w-full border">
+      <div v-for="project in projects" :key="project.id">
+        <img
+          :src="project.attributes.thumbnail.data.attributes.formats.medium.url"
+          alt=""
+        />
+      </div>
+    </div>
   </div>
- </div>
 </template>
 
-<script setup></script>
+<script setup>
+import { onMounted, ref } from "vue";
+import strapi from "../strapi/strapi";
+
+//  get projects from server
+
+const projects = ref([]);
+
+const getProjects = async () => {
+  const res = await strapi.request("GET", "/projects?populate=deep").then((res) => {
+    projects.value = res.data;
+  });
+};
+
+onMounted(() => {
+  getProjects();
+});
+</script>
 
 <style lang="scss" scoped></style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,15 +1,15 @@
 <template>
- <div>
-  <TheHero></TheHero>
-  <TheProfile></TheProfile>
-  <TheWorks></TheWorks>
- </div>
+  <div>
+    <TheHero></TheHero>
+    <TheProfile></TheProfile>
+    <TheWorks></TheWorks>
+  </div>
 </template>
 
 <script setup>
- import TheHero from "../components/TheHero.vue"
- import TheProfile from "../components/TheProfile.vue"
- import TheWorks from "../components/TheWorks.vue"
+import TheHero from "../components/TheHero.vue";
+import TheProfile from "../components/TheProfile.vue";
+import TheWorks from "../components/TheWorks.vue";
 </script>
 
 <style lang="scss" scoped></style>
