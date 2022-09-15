8f560cf0903844ec10a13b8777871186f23ef39f
jibril <jazimjibril@gmail.com>
2022-09-15 13:17:16 +0300

    made some changes

--- src/components/TheNav.vue
+++ src/components/TheNav.vue
@@ -1,15 +1,17 @@
 <template>
- <div>
-  <nav class="border flex justify-center text-2xl text-white py-1">
-   <RouterLink to="/" class="px-14">WORK</RouterLink>
-   <RouterLink to="/about" class="px-14">DEEP DIVE</RouterLink>
-   <RouterLink to="/about" class="px-14">JOURNAL</RouterLink>
-  </nav>
- </div>
+  <div>
+    <nav
+      class="border flex justify-center text-2xl text-white py-1 hover:text-fuchsia-600"
+    >
+      <RouterLink to="/" class="px-14">WORK</RouterLink>
+      <RouterLink to="/about" class="px-14">DEEP DIVE</RouterLink>
+      <RouterLink to="/about" class="px-14">JOURNAL</RouterLink>
+    </nav>
+  </div>
 </template>
 
 <script setup>
- import { RouterLink } from "vue-router"
+import { RouterLink } from "vue-router";
 </script>
 
 <style lang="scss" scoped></style>
