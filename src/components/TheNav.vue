510829186e17333ccb481567f874e1bbe1af0218
jibril <jazimjibril@gmail.com>
2022-09-15 17:51:34 +0300

    made some changes

--- src/components/TheNav.vue
+++ src/components/TheNav.vue
@@ -1,8 +1,6 @@
 <template>
   <div>
-    <nav
-      class="border flex justify-center text-2xl text-white py-1 hover:text-fuchsia-600"
-    >
+    <nav class="border flex justify-center text-2xl text-white py-1">
       <RouterLink to="/" class="px-14">WORK</RouterLink>
       <RouterLink to="/about" class="px-14">DEEP DIVE</RouterLink>
       <RouterLink to="/about" class="px-14">JOURNAL</RouterLink>
