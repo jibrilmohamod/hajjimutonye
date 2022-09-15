cf2983f5e3e199c092f5ae3f1147491be08ec37e
jibril <jazimjibril@gmail.com>
2022-09-15 17:51:44 +0300

    made some changes

--- src/views/WorkView.vue
+++ src/views/WorkView.vue
@@ -1,15 +1,9 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
+  <div>
+    <h1>MY WORK</h1>
   </div>
 </template>
 
-<style>
-@media (min-width: 1024px) {
-  .about {
-    min-height: 100vh;
-    display: flex;
-    align-items: center;
-  }
-}
-</style>
+<script setup></script>
+
+<style lang="scss" scoped></style>
