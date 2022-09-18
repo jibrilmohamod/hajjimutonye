f6818c39a967d7b04865dfa7ef2029a66e703a03
jibril <jazimjibril@gmail.com>
2022-09-18 14:32:14 +0300

    added deep dive view

--- src/router/index.js
+++ src/router/index.js
@@ -1,21 +1,27 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import { createRouter, createWebHistory } from "vue-router"
+import HomeView from "../views/HomeView.vue"
 import WorkView from "../views/WorkView.vue"
+import Deepiew from "../views/DeepView.vue"
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [
-    {
-      path: '/',
-      name: 'home',
-      component: HomeView
-    },
-    {
-      path: '/works',
-      name: 'works',
-      component: WorkView
-    }
-  ]
+ history: createWebHistory(import.meta.env.BASE_URL),
+ routes: [
+  {
+   path: "/",
+   name: "home",
+   component: HomeView,
+  },
+  {
+   path: "/works",
+   name: "works",
+   component: WorkView,
+  },
+  {
+   path: "/deepdive",
+   name: "deepdive",
+   component: Deepiew,
+  },
+ ],
 })
 
 export default router
