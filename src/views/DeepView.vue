8adc25b4efbafa215c7a38031d26225061d683d6
jibril <jazimjibril@gmail.com>
2022-09-18 21:44:11 +0300

    made some changes

--- src/views/DeepView.vue
+++ src/views/DeepView.vue
@@ -1,7 +1,27 @@
 <template>
-  <div></div>
+  <div class="bg-zinc-900">
+    <TheNav />
+    <div class="w-full pt-10">
+      <TheHeader>DEEP DIVE</TheHeader>
+      <p class="text-2xl text-yellow-50 text-center pt-3">
+        Get more insight on my projects and the inspiration behind them.
+      </p>
+    </div>
+    <div>{{ projects }}</div>
+  </div>
 </template>
 
-<script setup></script>
+<script setup>
+import TheHeader from "../components/ui/TheHeader.vue";
+import TheNav from "../components/TheNav.vue";
+import { useProjects } from "../stores/projects";
+import { ref } from "vue";
+
+const data = useProjects();
+const projects = ref(null);
+projects.value = data.projects;
+
+console.log(data);
+</script>
 
 <style lang="scss" scoped></style>
