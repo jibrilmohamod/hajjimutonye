f73a999f74a6f9ebfea9e0cef5c71d7c4ebf42fb
jibril <jazimjibril@gmail.com>
2022-09-18 21:43:49 +0300

    made some changes

--- src/components/TheNav.vue
+++ src/components/TheNav.vue
@@ -2,7 +2,7 @@
   <div>
     <nav class="flex justify-center text-2xl text-white py-1">
       <RouterLink to="/works" class="px-14">WORK</RouterLink>
-      <RouterLink to="/" class="px-14">DEEP DIVE</RouterLink>
+      <RouterLink to="/deepdive" class="px-14">DEEP DIVE</RouterLink>
       <RouterLink to="/" class="px-14">JOURNAL</RouterLink>
     </nav>
   </div>
