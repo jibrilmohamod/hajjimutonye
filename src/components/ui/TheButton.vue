e6dfef880f423e9df9cedb86cce62030c770a407
jibril <jazimjibril@gmail.com>
2022-09-19 19:19:03 +0300

    made some changes

--- src/components/ui/TheButton.vue
+++ src/components/ui/TheButton.vue
@@ -1,9 +1,16 @@
 <template>
- <button class="bg-white rounded px-5 py-3 mb-36 text-xl">
-  <slot />
- </button>
+  <router-link to="" class="bg-white rounded px-5 py-3 mb-36 text-xl">
+    <slot />
+  </router-link>
 </template>
 
-<script setup></script>
+<script setup>
+defineProps({
+  to: {
+    type: String,
+    required: true,
+  },
+});
+</script>
 
 <style lang="scss" scoped></style>
