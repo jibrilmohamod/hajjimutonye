5eb43a6d03f762f83ae2793059b718feaa0d9cc3
Jibril <jazimjibril@gmail.com>
2022-09-22 21:22:34 +0300

    made some changes

--- src/components/TheProfile.vue
+++ src/components/TheProfile.vue
@@ -1,34 +1,37 @@
 <template>
- <div class="h-screen bg-zinc-900 w-full px-32 pt-20">
-  <!-- name and img -->
+  <div class="h-screen bg-zinc-900 w-full px-32 pt-20">
+    <!-- name and img -->
 
-  <p class="text-5xl text-white">WHO IS HAJI MUTONYE?</p>
-  <div class="h-96 w-96 bg-slate-400 rounded-full ml-28 my-6">
-   <img src="" alt="" class="h-full w-full rounded-full" />
+    <p class="text-5xl text-white">WHO IS HAJI MUTONYE?</p>
+    <div class="h-96 w-96 bg-slate-400 rounded-full ml-28 my-6">
+      <img
+        src="../assets/images/profile.jpg"
+        alt=""
+        class="h-full w-full rounded-full object-cover object-center border"
+      />
+    </div>
+
+    <!-- about -->
+
+    <div class="w-1/2 text-yellow-50 text-xl mt-9">
+      <p>
+        Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod
+        tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At
+        vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren,
+        no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit
+        amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut.
+      </p>
+    </div>
+
+    <!-- call to action -->
+    <div class="w-full flex justify-center items-end mt-10">
+      <TheButton> GET IN TOUCH </TheButton>
+    </div>
   </div>
-
-  <!-- about -->
-
-  <div class="w-1/2 text-yellow-50 text-xl mt-9">
-   <p>
-    Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy
-    eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam
-    voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet
-    clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit
-    amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam
-    nonumy eirmod tempor invidunt ut.
-   </p>
-  </div>
-
-  <!-- call to action -->
-  <div class="w-full flex justify-center items-end mt-10">
-   <TheButton> GET IN TOUCH </TheButton>
-  </div>
- </div>
 </template>
 
 <script setup>
- import TheButton from "./ui/TheButton.vue"
+import TheButton from "./ui/TheButton.vue";
 </script>
 
 <style lang="scss" scoped></style>
