62bb9fefd86e4587beb3c4277f19e280d1cf3d14
Jibril <jazimjibril@gmail.com>
2022-09-24 23:33:07 +0300

    made some changes

--- src/views/JournalView.vue
+++ src/views/JournalView.vue
@@ -1,7 +1,18 @@
 <template>
-  <div></div>
+  <div class="bg-zinc-900">
+    <TheNav />
+    <div class="w-full pt-10">
+      <TheHeader>JOURNAL</TheHeader>
+      <p class="text-2xl text-yellow-50 text-center pt-3">
+        Get more insight on my projects and the inspiration behind them.
+      </p>
+    </div>
+  </div>
 </template>
 
-<script setup></script>
+<script setup>
+import TheNav from "../components/TheNav.vue";
+import TheHeader from "../components/ui/TheHeader.vue";
+</script>
 
 <style lang="scss" scoped></style>
