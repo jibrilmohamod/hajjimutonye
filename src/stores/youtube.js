4bc7d6b4f2b725f63ad26d8f2e5267528dbf317e
jibril <jazimjibril@gmail.com>
2022-09-25 01:18:09 +0300

    made some changes

--- src/stores/youtube.js
+++ src/stores/youtube.js
@@ -1,1 +1,19 @@
 import { defineStore } from "pinia";
+import strapi from "../strapi/strapi";
+import { ref } from "vue";
+
+
+export const useYoutube = defineStore('videos', () => {
+const videos = ref(null)
+
+async function getVideos() {
+    await strapi.request("GET", "/journals").then((res) => {
+     videos.value = res.data
+    })
+}
+
+getVideos()
+
+return {videos , getVideos}
+
+})
