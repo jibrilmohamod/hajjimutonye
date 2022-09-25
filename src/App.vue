bb5f87abc379639e44f5df7098596e1e53e1f790
jibril <jazimjibril@gmail.com>
2022-09-25 10:39:28 +0300

    made some changes

--- src/App.vue
+++ src/App.vue
@@ -11,12 +11,25 @@ import TheFooter from "./components/TheFooter.vue";
     </header>
   </main>
 
-  <RouterView />
+  <router-view v-slot="{ Component }">
+    <transition name="fade">
+      <component :is="Component" />
+    </transition>
+  </router-view>
   <TheFooter />
 </template>
 
 <style>
 #app {
   font-family: "Cartograph CF", sans-serif;
+}
+.fade-enter-active,
+.fade-leave-active {
+  transition: opacity 0.6s ease;
+}
+
+.fade-enter-from,
+.fade-leave-to {
+  opacity: 0;
 }
 </style>
