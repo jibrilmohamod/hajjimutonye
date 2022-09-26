1242b367630adbbeb57700a3fc6de3021f221096
jibril <jazimjibril@gmail.com>
2022-09-26 18:21:10 +0300

    made some changes

--- src/main.js
+++ src/main.js
@@ -4,6 +4,7 @@ import { VueShowdownPlugin } from "vue-showdown"
 import iVueFrame from "i-vue-frame"
 import VueAnimateOnScroll from "vue3-animate-onscroll"
 import VAnimateCss from "v-animate-css"
+import { createHead } from "@vueuse/head"
 
 import App from "./App.vue"
 import router from "./router"
@@ -11,6 +12,7 @@ import './assets/tailwind.css'
 
 
 const app = createApp(App)
+const head = createHead()
 
 app.use(createPinia())
 app.use(router)
@@ -25,6 +27,8 @@ app.use(VueShowdownPlugin, {
 app.use(iVueFrame)
 app.use(VueAnimateOnScroll)
 app.use(VAnimateCss)
+app.use(head)
+
 
 
 
