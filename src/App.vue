8fa850ad22b0d8fd5517248c38c7738619ea2161
jibril <jazimjibril@gmail.com>
2022-09-26 18:21:04 +0300

    made some changes

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,23 @@
 import { RouterView } from "vue-router";
 import TheName from "./components/TheName.vue";
 import TheFooter from "./components/TheFooter.vue";
+import { useHead } from "@vueuse/head";
+import { computed, reactive } from "vue";
+
+const siteData = reactive({
+  title: `Haji Mutonye`,
+  description: `Haji Mutonye is a Film-maker and concept creator from Nairobi,Kenya`,
+});
+
+useHead({
+  title: computed(() => siteData.title),
+  meta: [
+    {
+      name: `description`,
+      content: computed(() => siteData.description),
+    },
+  ],
+});
 </script>
 
 <template>
