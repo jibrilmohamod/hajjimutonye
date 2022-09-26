ae3f41981e6c14912c766a7407de840831735133
jibril <jazimjibril@gmail.com>
2022-09-26 19:33:56 +0300

    made some changes

--- src/components/TheFooter.vue
+++ src/components/TheFooter.vue
@@ -1,9 +1,28 @@
 <template>
   <div class="bg-zinc-900 w-full text-yellow-50">
     <!-- contact details -->
-    <div class="flex text-xl px-16 gap-10 border">
+    <div class="flex flex-col text-5xl px-16 gap-5 items-center">
       <h1>CONTACT ME</h1>
-      <ul class="border flex"></ul>
+      <ul class="flex w-2/4 justify-center gap-10 text-4xl">
+        <li>
+          <a href="mailto:haji@hajimutonye.com" class="hover:text-red-600"
+            ><i class="fas fa-envelope"></i
+          ></a>
+        </li>
+        <li>
+          <a href="https://www.instagram.com/hajimutonye/" class="hover:text-pink-700"
+            ><i class="fab fa-instagram"></i
+          ></a>
+        </li>
+        <li>
+          <!-- twitter -->
+          <a href="https://twitter.com/hajjimutonye" class="hover:text-cyan-500"
+            ><i class="fab fa-twitter"></i
+          ></a>
+        </li>
+      </ul>
+      <!-- copyright -->
+      <p class="text-sm">© 2022 Haji Mutonye</p>
     </div>
     <router-link to="/">
       <img src="../assets/images/footers.svg" alt="" class="bg-zinc-900 w-screen" />
@@ -19,6 +38,9 @@
     flex-direction: column;
     align-items: center;
     gap: 5px;
+    h1 {
+      font-size: 1.6rem;
+    }
     ul {
       list-style: none;
       li {
