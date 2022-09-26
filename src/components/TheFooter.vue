38c77c62d737af0b170cfd54f090a843d97628e1
jibril <jazimjibril@gmail.com>
2022-09-26 09:42:15 +0300

    made some changes

--- src/components/TheFooter.vue
+++ src/components/TheFooter.vue
@@ -1,33 +1,46 @@
 <template>
- <div class="bg-zinc-900 w-full text-yellow-50">
-  <!-- contact details -->
-  <div class="flex text-xl px-16 gap-10">
-   <h1>CONTACT ME</h1>
-   <ul>
-    <li>
-     <a href=""> <i class="pr-5 fas fa-envelope"></i>Info@hajimutonye.com </a>
-    </li>
-    <li>
-     <a href=""><i class="pr-5 fab fa-youtube"></i>HAJIMUTONYE</a>
-    </li>
-    <li>
-     <a href=""><i class="pr-5 fab fa-instagram"></i>HAJIMUTONYE</a>
-    </li>
-    <li>
-     <a href=""><i class="pr-5 fab fa-twitter"></i>HAJIMUTONYE</a>
-    </li>
-   </ul>
+  <div class="bg-zinc-900 w-full text-yellow-50">
+    <!-- contact details -->
+    <div class="flex text-xl px-16 gap-10">
+      <h1>CONTACT ME</h1>
+      <ul>
+        <li>
+          <a href=""> <i class="pr-5 fas fa-envelope"></i>Info@hajimutonye.com </a>
+        </li>
+        <li>
+          <a href=""><i class="pr-5 fab fa-youtube"></i>HAJIMUTONYE</a>
+        </li>
+        <li>
+          <a href=""><i class="pr-5 fab fa-instagram"></i>HAJIMUTONYE</a>
+        </li>
+        <li>
+          <a href=""><i class="pr-5 fab fa-twitter"></i>HAJIMUTONYE</a>
+        </li>
+      </ul>
+    </div>
+    <router-link to="/">
+      <img src="../assets/images/footers.svg" alt="" class="bg-zinc-900 w-screen" />
+    </router-link>
   </div>
-  <router-link to="/">
-   <img
-    src="../assets/images/footers.svg"
-    alt=""
-    class="bg-zinc-900 w-screen"
-   />
-  </router-link>
- </div>
 </template>
 
 <script setup></script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+@media screen and (max-width: 40em) {
+  div {
+    flex-direction: column;
+    align-items: center;
+    gap: 5px;
+    ul {
+      list-style: none;
+      li {
+        a {
+          text-decoration: none;
+          font-size: 15px;
+        }
+      }
+    }
+  }
+}
+</style>
