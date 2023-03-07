9b8a7f5c5d2d12acb7bc6de7b36c47d3f774de76
Jibril <jazimjibril@gmail.com>
2023-03-07 23:38:04 +0300

    asfsdfsdfdsfdf

--- src/views/ImagesView.vue
+++ src/views/ImagesView.vue
@@ -2,7 +2,26 @@
   <div class="bg-zinc-900 text-white pb-16">
     <TheNav />
 
-    {{ currentProject.images }}
+    <div class="w-[70vw] m-auto">
+      <!-- project title -->
+      <h1 class="text-5xl text-center pt-10">
+        {{ currentProject.title }}
+      </h1>
+      <!-- project description -->
+      <p class="w-1/2 m-auto text-2xl text-left pt-10">
+        {{ currentProject.description }}
+      </p>
+      <!-- project images -->
+      <div class="w-full flex flex-wrap justify-center gap-10 pt-10">
+        <div v-for="image in currentProject.images"
+             :key="image.id"
+             class="w-full h-full rounded-md overflow-hidden">
+          <img :src="image"
+               alt=""
+               class="w-full h-full object-cover" />
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
