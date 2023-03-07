326e509624a5ec563ad906d43eecf25281b124b9
Jibril <jazimjibril@gmail.com>
2023-03-07 23:31:22 +0300

    fdfsdfsdafasdfsd

--- src/views/ImagesView.vue
+++ src/views/ImagesView.vue
@@ -2,45 +2,23 @@
   <div class="bg-zinc-900 text-white pb-16">
     <TheNav />
 
-    <!-- images and description -->
-    <div class="w-[70vw] m-auto">
-      <!-- project title -->
-      <h1 class="text-5xl text-center pt-10">
-        {{ currentProject.attributes.title }}
-      </h1>
-      <!-- project description -->
-      <p class="w-1/2 m-auto text-2xl text-left pt-10">
-        {{ currentProject.attributes.description }}
-      </p>
-      <!-- project images -->
-      <div class="w-full flex flex-wrap justify-center gap-10 pt-10">
-        <div
-          v-for="image in currentProject.attributes.images.data"
-          :key="image.id"
-          class="w-full h-full rounded-md overflow-hidden"
-        >
-          <img :src="image.attributes.url" alt="" class="w-full h-full object-cover" />
-        </div>
-      </div>
-    </div>
+    {{ currentSlug }}
   </div>
 </template>
 
 <script setup>
-import { computed } from "vue";
+import { ref } from "vue";
 import { useRoute } from "vue-router";
 import TheNav from "../components/TheNav.vue";
 
 //  get projects from server
-import { useProjects } from "../stores/projects";
-const data = useProjects();
+import data from "../assets/projects.json";
+const projects = ref(data.projects);
 
 // get project by slug
 const route = useRoute();
 const currentSlug = route.params.slug;
-const currentProject = computed(() => {
-  return data.projects.find((project) => project.attributes.slug === currentSlug);
-});
+const currentProject = ''
 </script>
 
 <style lang="scss" scoped>
@@ -51,9 +29,11 @@ const currentProject = computed(() => {
     padding-left: 7px;
     margin: auto;
   }
+
   div {
     width: 100%;
   }
+
   h1 {
     font-size: 30px;
   }
